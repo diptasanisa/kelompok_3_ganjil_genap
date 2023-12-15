2c31aad57469af4cb7f59205dd4bd8bd3084cabe
LAPTOP-VG1P2RSJ\ASUS <pradipta.shofianisa20@student.uisi.ac.id>
2023-12-15 16:27:48 +0700

    Penambahan test case

--- test_ganjil_genap.py
+++ test_ganjil_genap.py
@@ -5,3 +5,12 @@ def test_ganjil():
 
 def test_genap():
     assert cek_ganjil_genap(4) == "Genap"
+
+def test_ganjil_salah():
+    assert not cek_ganjil_genap(4) == "Ganjil"
+
+def test_0_genap():
+    assert cek_ganjil_genap(0) == "Genap"
+    
+def test_minus_ganjil():
+    assert cek_ganjil_genap(-1) == "Ganjil"
